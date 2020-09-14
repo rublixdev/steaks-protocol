027aee37fe6c8a02ebee2a4c740d56a00ed96bb1
Furion <_@furion.me>
2020-09-14 21:32:11 +0200

    Rename LP token for clarity

--- contracts/uniswapv2/UniswapV2ERC20.sol
+++ contracts/uniswapv2/UniswapV2ERC20.sol
@@ -6,7 +6,7 @@ contract UniswapV2ERC20 {
     using SafeMathUniswap for uint;
 
     string public constant name = 'SteakSwap LP Token';
-    string public constant symbol = 'SLP';
+    string public constant symbol = 'STEAK-LP';
     uint8 public constant decimals = 18;
     uint  public totalSupply;
     mapping(address => uint) public balanceOf;
